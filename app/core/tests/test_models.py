bf9b980270f0bfa4a05b364fac1ca5e60803ace9
hli <harveyli88@utexas.edu>
2021-11-16 18:01:57 -0600

    Configured postgres DB

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.contrib.auth import get_user, get_user_model
 
 
-
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -23,7 +22,6 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
-
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
